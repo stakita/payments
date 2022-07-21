6b7e110424a39c4a228a2d2e13513c1ba2579119
Simon Takita <smtakita@gmail.com>
2022-07-21 07:35:21 -0700

    Basic file line processing without csv

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+use std::io::prelude::*;
 use std::error::Error;
+use std::io::BufReader;
+use std::fs::File;
+use serde::Deserialize;
 
 pub struct Config {
     pub filename: String,
@@ -23,5 +27,45 @@ impl Config {
 
 pub fn run(config:Config) -> Result<(), Box<dyn Error>> {
     println!("running with filename '{}'", config.filename);
+
+    // let mut reader = csv::Reader::from_reader(io::std)
+    read_iter(&config.filename, process_line)
+
+    // Ok(())
+}
+
+fn read_iter(filename: &str, func: fn(&str)) -> Result<(), Box<dyn Error>> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        func(&line.unwrap());
+    }
+
     Ok(())
 }
+
+fn process_line(line: &str) {
+    println!("line: {}", line);
+}
+
+#[derive(Debug, Deserialize)]
+enum TransactionType {
+    Deposit,
+    Withdrawal,
+    Dispute,
+    Resolve,
+    Chargeback
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Transaction {
+    tx_type: TransactionType,
+    client_id: u16,
+    tx_id: u32,
+    amount: Option<f64>,
+}
+
+// impl Transaction {
+
+// }
