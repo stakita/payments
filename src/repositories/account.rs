433358369e5bb9fcd36b56756385a2962ae6f587
Simon Takita <smtakita@gmail.com>
2022-07-25 17:13:42 -0700

    Add in accounts repository code

---
+++ src/repositories/account.rs
@@ -0,0 +1,15 @@
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Account {
+    pub client_id: u16,
+    pub available: f64,
+    pub held: f64,
+    pub total: f64,
+    pub locked: bool,
+}
+
+pub trait AccountRepositoryTrait {
+    fn insert(&mut self, account: Account);
+    fn find(&mut self, client_id: u16) -> Option<&Account>;
+    fn find_all(&mut self) -> Vec<&Account>;
+}
---
+++ src/repositories/account_in_memory.rs
@@ -0,0 +1,115 @@
+
+use std::collections::HashMap;
+use itertools::Itertools;
+use crate::repositories::account::{
+    Account,
+    AccountRepositoryTrait
+};
+
+pub struct AccountRepositoryInMemory {
+    pub store: HashMap<u16, Account>,
+}
+
+impl AccountRepositoryInMemory {
+    pub fn new() -> AccountRepositoryInMemory {
+        AccountRepositoryInMemory {
+            store: HashMap::new(),
+        }
+    }
+}
+
+impl AccountRepositoryTrait for AccountRepositoryInMemory {
+
+        fn insert(&mut self, account: Account) {
+            let _ = &self.store.insert(account.client_id, account);
+        }
+
+        fn find(&mut self, client_id: u16) -> Option<&Account> {
+            self.store.get(&client_id)
+        }
+
+        fn find_all(&mut self) -> Vec<&Account> {
+            let mut elements = Vec::<&Account>::new();
+            for key in self.store.keys().sorted() {
+                elements.push(self.store.get(key).unwrap());
+            }
+            elements
+        }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_can_insert_and_find() {
+        let mut ar = AccountRepositoryInMemory::new();
+        let a = Account {
+            client_id: 42,
+            available: 1.23,
+            held: 0.0,
+            total: 1.23,
+            locked: false,
+        };
+        let b = Account {
+            client_id: 21,
+            available: 0.23,
+            held: 0.0,
+            total: 0.23,
+            locked: false,
+        };
+
+        AccountRepositoryTrait::insert(&mut ar, a.clone());
+        AccountRepositoryTrait::insert(&mut ar, b.clone());
+
+        // it finds an inserted key
+        let res = AccountRepositoryTrait::find(&mut ar, a.client_id).unwrap();
+        assert_eq!(res, &a);
+
+        // it finds an inserted key
+        let res = AccountRepositoryTrait::find(&mut ar, b.client_id).unwrap();
+        assert_eq!(res, &b);
+
+        // it fails to find an invalid key
+        let res = AccountRepositoryTrait::find(&mut ar, 68);
+        assert_eq!(res, None);
+
+    }
+
+    #[test]
+    fn it_can_insert_and_find_all() {
+        let mut ar = AccountRepositoryInMemory::new();
+        let a = Account {
+            client_id: 42,
+            available: 1.23,
+            held: 0.0,
+            total: 1.23,
+            locked: false,
+        };
+        let b = Account {
+            client_id: 420,
+            available: 10.23,
+            held: 0.0,
+            total: 10.23,
+            locked: true,
+        };
+        let c = Account {
+            client_id: 1,
+            available: 1.11,
+            held: 1.22,
+            total: 1.33,
+            locked: true,
+        };
+
+        AccountRepositoryTrait::insert(&mut ar, a);
+        AccountRepositoryTrait::insert(&mut ar, b);
+        AccountRepositoryTrait::insert(&mut ar, c);
+
+        let res = AccountRepositoryTrait::find_all(&mut ar);
+        println!("res: {:?}", res);
+
+        // assert_eq!(res, &a);
+    }
+
+}
